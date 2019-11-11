e0f87141653706f01be11a3d0eb17ce0964abb9a
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-11-11 15:20:24 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w( Fiction Non-Fiction)}
+    validates :not_clickbaity
+
+    def not_clickbaity
+        if !title.include?("Won't Believe") || !title.include?("Secret") || !title.include?("Top [number]") || !title.include?("Guess")
+            errors.add(:title, "Not click baity")
+        end
+    end
+
+
 end
